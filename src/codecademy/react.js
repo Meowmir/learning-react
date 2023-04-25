b8560bdb722b5445cf8cf7fa6b6fe1833b0cda95
Meowmir <disewji@gmail.com>
2023-04-25 10:47:02 +0200

    Testing out the terminal

--- src/codecademy/react.js
+++ src/codecademy/react.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function FirstComponent() {
-  return <h1>I learned this from Codecademy!</h1>;
+  return (<h1>I learned this from Codecademy!</h1>), SecondComp();
 }
 
 export default FirstComponent;
+
+const Nora = {
+  name: "Nora Disewji",
+  status: "Best",
+};
+
+function SecondComp() {
+  return (
+    <div>
+      <h1>{Nora.name} </h1>
+      <p>{Nora.status}</p>
+    </div>
+  );
+}
