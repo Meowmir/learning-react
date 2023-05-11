b801794a590652859c5b9bb30b9b3aa12e408682
Meowmir <disewji@gmail.com>
2023-05-11 08:32:33 +0200

    fixed before and after with help from Hugo

--- src/components/nora-textfield.tsx
+++ src/components/nora-textfield.tsx
@@ -48,9 +48,15 @@ const StyledTextArea = styled.textarea`
   }
 
   &.textfield__variant--filled {
+    padding-top: 25px;
+    padding-right: 12px;
+    padding-bottom: 8px;
+    padding-left: 12px;
+
     background-color: #0000000f;
     color: #000000de;
     display: inline-flex;
+    border: 0;
 
     &:hover {
       background-color: #00000017;
@@ -62,13 +68,50 @@ const StyledTextArea = styled.textarea`
   }
 
   &.textfield__variant--standard {
+    font: inherit;
+    letter-spacing: inherit;
+    color: currentColor;
+    padding: 4px 0 5px;
+    border: 0;
+    box-sizing: content-box;
+    background: none;
+    height: 1.4375em;
+    margin: 0;
+    -webkit-tap-highlight-color: transparent;
+    display: block;
+    min-width: 0;
+    width: 100%;
+    animation-duration: 10ms;
   }
 `;
 
 const StyledInput = StyledTextArea.withComponent("input");
 
+const StyledDiv = styled.div`
+  &::before {
+    content: "";
+    border-bottom: 1px solid #ff00ff;
+    left: 0;
+    bottom: 0;
+    position: absolute;
+    right: 0;
+    transition: border-bottom-color 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    pointer-events: none;
+  }
+
+  &::after {
+    content: "";
+    border-bottom: 2px solid #ff00ff;
+  }
+
+  /* inside this div is another element with this class */
+  &.special-class .textfield__variant--filled {
+  }
+`;
+
 export function NoraTextField(props: TextFieldProps) {
   const classes: string[] = [];
+  const divClasses: string[] = [];
 
   const Elem = props.multiline ? StyledTextArea : StyledInput;
 
@@ -78,11 +121,16 @@ export function NoraTextField(props: TextFieldProps) {
 
   if (props.variant === "filled") {
     classes.push("textfield__variant--filled");
+    divClasses.push("textfield__variant--filled");
   }
 
   if (props.variant === "standard") {
     classes.push("textfield__variant--standard");
   }
 
-  return <Elem className={classes.join(" ")} />;
+  return (
+    <StyledDiv className={divClasses.join(" ")}>
+      <Elem className={classes.join(" ")} />
+    </StyledDiv>
+  );
 }
